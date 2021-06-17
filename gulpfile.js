c88e0e7ee6048df18b6925ee2141f7ba7459fb22
fdelillo <federicodelillo@gmail.com>
2021-06-17 08:59:30 -0300

    Proyecto finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,60 @@
-const {series, src, dest, watch} = require('gulp');
+const {series, src, dest, watch, parallel} = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
+const concat = require('gulp-concat');
+
+const paths = {
+    imagenes: "src/img/**/*",
+    scss: "src/scss/**/*.scss",
+    js: 'src/js/**/*.js'
+}
 
 function css() {
-    return src("./src/scss/app.scss")
-        .pipe(sass())
+    return src(paths.scss)
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
         .pipe(dest("./build/css"));
 }
 
 function minificarCss() {
-    return src("./src/scss/app.scss")
+    return src(paths.scss)
         .pipe(sass({
             outputStyle: 'compressed'
         }))
         .pipe(dest("./build/css"));
 }
 
+function javascript() {
+    return src(paths.js)
+        .pipe(concat('bundle.js'))
+        .pipe(dest('./build/js'))
+}
+
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe(imagemin())
+        .pipe(dest("./build/img"))
+        //.pipe(notify({message: 'Imagen Minificada'}));
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe(webp())
+        .pipe(dest("./build/img"))
+        //.pipe(notify({message: 'Imagen webPlista'}));
+}
+
 function watchArchivo() {
-    watch('./src/scss/**/*.scss', css);
+    watch(paths.scss, css);
+    watch(paths.js, javascript);
 }
 
 exports.css = css;
 exports.minificarCss = minificarCss;
+exports.imagenes = imagenes;
 exports.watchArchivo = watchArchivo;
+
+exports.default = series(css,javascript,imagenes,versionWebp,watchArchivo);
